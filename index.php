f496955567fa931cdd51afc7d3dc6dc9d4a7e8f2
Remy Sharp <remy@leftlogic.com>
2010-02-23 01:03:36 +0000

    Finished homepage (though static tweets) for redesign version 2 - only tested in Safari & Firefox so far. Expecting tweaks for IE.

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <?php
-include('auth/secret.php');
-include('auth/php/EpiCurl.php');
-include('auth/php/EpiOAuth.php');
-include('auth/php/EpiTwitter.php');
-
-$twitterObj = new EpiTwitter(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
-$twitterInfo = null;
-
-if ($_COOKIE['token'] && file_exists('auth/oauth/' . $_COOKIE['token'])) {
-	$username = file_get_contents('auth/oauth/' . $_COOKIE['token']);
-	$userToken = $_COOKIE['token'];
-	$userSecret = file_get_contents('auth/oauth/' . $username . '-sec');
-
-	$twitterObj->setToken($userToken, $userSecret);
-	$twitterInfo = $twitterObj->get_accountVerify_credentials();
-	
-	if (!$twitterInfo->screen_name) {
-      // reset 
-      $twitterObj = new EpiTwitter(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
-      $twitterInfo = null;
-  }  
-}
-
-$screen_name = isset($_GET['screen_name']) ? $_GET['screen_name'] : '';
-$search = isset($_GET['search']) ? $_GET['search'] : '';
-$type = isset($_GET['type']) ? $_GET['type'] : 'timeline';
-
-if ($type == 'list') {
-  $type = 'timeline';
-}
+// include('auth/secret.php');
+// include('auth/php/EpiCurl.php');
+// include('auth/php/EpiOAuth.php');
+// include('auth/php/EpiTwitter.php');
+// 
+// $twitterObj = new EpiTwitter(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
+// $twitterInfo = null;
+// 
+// if ($_COOKIE['token'] && file_exists('auth/oauth/' . $_COOKIE['token'])) {
+//  $username = file_get_contents('auth/oauth/' . $_COOKIE['token']);
+//  $userToken = $_COOKIE['token'];
+//  $userSecret = file_get_contents('auth/oauth/' . $username . '-sec');
+// 
+//  $twitterObj->setToken($userToken, $userSecret);
+//  $twitterInfo = $twitterObj->get_accountVerify_credentials();
+//  
+//  if (!$twitterInfo->screen_name) {
+//       // reset 
+//       $twitterObj = new EpiTwitter(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
+//       $twitterInfo = null;
+//   }  
+// }
+// 
+// $screen_name = isset($_GET['screen_name']) ? $_GET['screen_name'] : '';
+// $search = isset($_GET['search']) ? $_GET['search'] : '';
+// $type = isset($_GET['type']) ? $_GET['type'] : 'timeline';
+// 
+// if ($type == 'list') {
+//   $type = 'timeline';
+// }
 
 ?>
 <!DOCTYPE html>
@@ -42,7 +42,7 @@ if ($type == 'list') {
 -->
 <html lang="en">
 <head>
-<title>Snap Bird - re-finding the ones that got away</title>
+<title>Snap Bird - search Twitter's history</title>
 <meta charset="utf-8" />
 <link rel="shortcut icon" href="/images/snapbird-icon.png" />
 <link rel="apple-touch-icon" href="/images/snapbird-icon.png" />
@@ -52,67 +52,130 @@ if ($type == 'list') {
 <link rel="stylesheet" href="/snapbird.css" type="text/css" />
 </head>
 <body>
+  <div id="content">
   <header>
     <h1><a href="/">Snap Bird</a></h1>
-    <p>Re-finding the ones that got away</p>
+    <h2>Search beyond Twitter's history</h2>
   </header>
-  <article id="main">
-    <p>Can&rsquo;t remember who said that thing that time?</p>
-    <ul class="plain">
-      <li>Search <strong>beyond</strong> Twitter&rsquo;s 10-day search archive</li>
-      <li>Search <strong>tweets, lists, friends &amp; favourites</strong></li>
-    </ul>
-    <form action="/">
-      <fieldset class="who">
-        <legend>Where to search</legend>
+  <form>
+    <article id="controls">
+      <fieldset>
         <div>
-          <label for="screen_name">Search</label>
-          <?php if ($twitterInfo != null) : ?>
-          <span title="Searching friends is limited to your screen name" class="input" id="auth_screen_name"><?= $twitterInfo->screen_name ?></span>  
-          <?php endif ?>
-          <input type="text" name="screen_name" value="<?=$screen_name?>" id="screen_name" placeholder="screen name" />
-          <span>&rsquo;s</span>
+          <label for="type">Search</label>
+          <select name="type" id="type">
+            <option value="timeline">Someones timeline</option>
+            <option value="favs">Someones favourites</option>
+            <option value="with_friends">Your friends' tweets</option>
+            <option value="mentions">Tweets mentioning you</option>
+            <option value="dm_sent">Your sent direct messages</option>
+            <option value="dm_received">Your received direct messages</option>
+          </select>
         </div>
-        <div class="radiogroup">
-          <input type="radio" name="source" <?php if ($type == 'timeline') echo 'checked="checked" '; ?>value="timeline" id="timeline" /><label for="timeline">tweets</label>
-          <input type="radio" name="source" <?php if ($type == 'favs') echo 'checked="checked" '; ?>value="favs" id="favs" /><label for="favs">favourites</label>
-          <input class="authRequired" type="radio" name="source" <?php if ($type == 'withfriends') echo 'checked="checked" '; ?>value="withfriends" id="withfriends" /><label for="withfriends">friends&rsquo; tweets</label>
-        </div>
-        <div class="radiogroup">
-          <input class="authRequired" type="radio" name="source" <?php if ($type == 'dm_sent') echo 'checked="checked" '; ?>value="dm_sent" id="dm_sent" /><label for="dm_sent">sent DMs</label>
-          <input class="authRequired" type="radio" name="source" <?php if ($type == 'dm') echo 'checked="checked" '; ?>value="dm" id="dm" /><label for="dm">received DMs</label>
-        </div>
-      </fieldset>
-      <fieldset class="for">
-        <legend>What to search for</legend>
         <div>
-          <label for="search">for</label>
-          <input type="text" name="search" value="<?=$search?>" id="search" />
-          <p class="note">You can do all the usual search stuff like <br />wrapping &ldquo;phrases in quotes&rdquo; and using OR, etc.</p>
+          <label for="screen_name" id="screen_name_label">Who</label>
+          <input class="screen_name" type="text" id="screen_name" name="screen_name" placeholder="username" />
         </div>
-        <input type="submit" class="submit" value="Search" />
+        <div>
+          <label for="search">For</label>
+          <input type="text" id="search" name="search" placeholder="search term" />
+        </div>        
       </fieldset>
-    </form>
-  </article>
-  <div id="mainFooter"></div>
-  <article id="tweets">
-    <aside>
-      <p><a id="permalink" href="http://snapbird.org/">&raquo; Permalink to results</a></p>
-      <div id="status"></div>
-      <input type="button" class="search button" value="Search back further" />
+    </article>
+    <div id="worm-and-bird" class="showinfo">
+      <input type="submit" value="find it" id="findit-btn" />
+      <a id="login" href="<?='#'/*$twitterObj->getAuthorizationUrl()*/?>">
+        <span id="info"></span>
+        <div id="authenticate-info">
+          <p>Twitter allows only <em><strong>you</strong></em> to search within your direct messages, friends' tweets, etc.</p>
+          <p>Click here to let Twitter know we're authentically searching on your behalf.</p>
+        </div>
+        <span id="authenticate-button" class="button">Authenticate with <span id="twitter-logo">Twitter</span></span>        
+      </a>
+      <div id="bird"></div>
+    </div>
+  </form>
+  <article id="results">
+    <aside id="tweets_about_snapbird">
+      <h2>Tweets about Snap Bird</h2>
+      <ul>
+        <li>
+          <a href="#">
+          <img src="http://twivatar.org/rem/mini" />
+          Giggling at the spikes in traffic to http://isjeremyeatingtoast.com coinciding with @adactio's tweets: http://snapbird.org/adactio/toast
+          </a>
+        </li>
+        <li>
+          <img src="http://twivatar.org/xosecastro/mini" />
+          I've just found http://snapbird.org, a site to search tweets among the people you follow on Twitter. Thanks to Aardvark @vark
+        </li>
+        <li>
+          <img src="http://twivatar.org/shauninman/mini">
+          http://snapbird.org/ it is.
+        </li>
+        <li>
+          <img src="http://twivatar.org/rem/mini" />
+          Giggling at the spikes in traffic to http://isjeremyeatingtoast.com coinciding with @adactio's tweets: http://snapbird.org/adactio/toast
+        </li>
+        <li>
+          <img src="http://twivatar.org/xosecastro/mini" />
+          I've just found http://snapbird.org, a site to search tweets among the people you follow on Twitter. Thanks to Aardvark @vark
+        </li>
+      </ul>
     </aside>
-
-<ul>
-</ul>
-
-    <input type="button" class="search button" value="Search back further" />
+    <div id="intro">
+      <h2>Where can you search?</h2>
+      <table>
+        <thead>
+          <tr>
+            <th class="no where">Where</th>
+            <th class="twitter">Twitter Search</th>
+            <th class="snapbird">Snap Bird</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>The public timeline</td>
+            <td class="yes"></td>
+            <td class="no"></td>
+          </tr>
+          <tr>
+            <td>Further back than 10 days</td>
+            <td class="no"></td>
+            <td class="yes"></td>
+          </tr>
+          <tr>
+            <td>Within only your friends' tweets</td>
+            <td class="no"></td>
+            <td class="yes"></td>
+          </tr>
+          <tr>
+            <td>Within your Direct Messages</td>
+            <td class="no"></td>
+            <td class="yes"></td>
+          </tr>
+          <tr>
+            <td>Within any user's favourites</td>
+            <td class="no"></td>
+            <td class="yes"></td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <th></th>
+            <th class="twitter"></th>
+            <th class="snapbird"></th>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+    <div class="clear"></div>
   </article>
   <footer>
     <nav>
       <ul id="navlinks">
-        <li><a href="/">Search</a></li>
+        <li><a href="http://snapbird.org">Search Twitter's history</a></li>
         <!-- <li><a href="/about">About</a></li> --> <!-- Coming soon -->
-        <li><a href="http://github.com/remy/snapbird">Developers</a></li>
+        <li><a href="http://github.com/remy/snapbird">Fork me on Github</a></li>
         <?php if ($twitterInfo != null) : ?>
         <li><a id="logout" href="/">Logout</a></li>
         <?php endif ?>
@@ -121,7 +184,7 @@ if ($type == 'list') {
     <ul id="credit">
       <li><a href="http://twitter.com/rem">Built by @rem</a></li>
       <li><a href="http://twitter.com/nicepaul">Designed by @nicepaul</a></li>
-      <li><a href="http://twitter.com/stompfrog">bird by @stompfrog</a></li>
+      <li><a href="http://twitter.com/stompfrog">Illustrated @stompfrog</a></li>
     </ul>
   </footer>
   <div id="auth">
@@ -137,17 +200,9 @@ if ($type == 'list') {
       </div>
     </div>    
   </div>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script src="/twitterlib/twitterlib.js?2009-12-31"></script>
-<script src="/snapbird.js?2009-12-31"></script>
-<script>
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script>
-try {
-var pageTracker = _gat._getTracker("UA-1656750-19");
-pageTracker._trackPageview();
-} catch(err) {}</script>
+</div>
+<!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script> -->
+<!-- <script src="/twitterlib/twitterlib.js?2009-12-31"></script> -->
+<!-- <script src="/snapbird.js?2009-12-31"></script> -->
 </body>
 </html>
